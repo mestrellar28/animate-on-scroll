4faa98e9103e5613a26801df078a37637e13f573
Manuel Estrella <mestrellar28@gmail.com>
2021-07-13 16:28:09 +0200

    add the page styles and script to animate every element on scroll

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './main.scss';
+
+// select all the elements to animate
+let elements = document.querySelectorAll('.animate');
+
+// intersecion observer options
+let options = {
+   rootMargin: '-30%',
+   treshold: 0.9,
+};
+
+// intersection observer
+let observer = new IntersectionObserver(showEl, options);
+
+// add active class when every element is in the viewport
+function showEl(entries) {
+   entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+         entry.target.classList.add('active');
+      }
+   });
+}
+
+// observe every element
+elements.forEach((el) => {
+   observer.observe(el);
+});
+
+// Force to refresh page on top
+if (history.scrollRestoration) {
+   history.scrollRestoration = 'manual';
+} else {
+   window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+   };
+}
+
+window.onbeforeunload = function () {
+   window.scrollTo(0, 0);
+};
